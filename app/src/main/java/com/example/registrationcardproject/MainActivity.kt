6aca3910e1115a8b7417714eae7b492819ee6819
faisal <faisal.Nahi.ruh.k@tuwaiq.edu.sa>
2021-11-01 08:56:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/registrationcardproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.registrationcardproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.jar.Attributes
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+
+    }
+}
